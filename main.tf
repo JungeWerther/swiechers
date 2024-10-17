4084887d61284b9f9aefde319a3208438d2fe015
Seb Wiechers <sebastiaanwiechers@icloud.com>
2024-10-18 00:24:32 +0200

    working now?

--- main.tf
+++ main.tf
@@ -1,3 +1,15 @@
+terraform {
+  required_providers {
+    aws = {
+      source = "hashicorp/aws"
+      version = "5.72.1"
+    }
+  }
+}
+
+provider "aws" {
+  region = "eu-central-1"
+}
 
 module "lambda_example_container-image" {
   source  = "terraform-aws-modules/lambda/aws//examples/container-image"
