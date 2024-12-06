334e6c7d4896ee519c6ea7419d86d79f7a8aa08c
Seb Wiechers <sebastiaanwiechers@icloud.com>
2024-12-06 17:01:45 +0100

    og_image

--- front/src/app/layout.tsx
+++ front/src/app/layout.tsx
@@ -11,6 +11,21 @@ const font = DM_Mono({
 export const metadata: Metadata = {
   title: "Seb Wiechers",
   description: "Data Wizard, Software Engineer, Entrepreneur",
+  openGraph: {
+    type: "website",
+    locale: "en_US",
+    url: "https://swiechers.nl",
+    title: "Seb Wiechers",
+    description: "Data Wizard, Software Engineer, Entrepreneur",
+    images: [
+      {
+        url: "https://swiechers.nl/og_image.JPG",
+        width: 1200,
+        height: 800,
+        alt: "Seb Wiechers",
+      },
+    ],
+  },
 };
 
 export default function RootLayout({
